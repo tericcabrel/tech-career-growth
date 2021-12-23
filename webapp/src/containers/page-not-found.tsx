ddf48cb57d6f41f5f6b91c81b9b44fbf5c73205c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-12-23 17:56:34 +0100

    style(app): review the design of the not found page

--- webapp/src/containers/page-not-found.tsx
+++ webapp/src/containers/page-not-found.tsx
@@ -6,17 +6,12 @@ const PageNotFound = () => {
     <div className="bg-white py-6 sm:py-8 lg:py-12 h-screen flex items-center">
       <div className="max-w-screen-lg px-4 md:px-8 mx-auto">
         <div className="grid sm:grid-cols-2 gap-8">
-          <div className="h-80 md:h-auto bg-gray-100 overflow-hidden shadow-lg rounded-lg">
-            <Image
-              src="https://images.unsplash.com/photo-1626790680787-de5e9a07bcf2?auto=format&q=75&fit=crop&w=600"
-              loading="lazy"
-              alt="Photo by Theo Crazzolara"
-              className="w-full h-full object-cover object-center"
-            />
+          <div className="flex items-center overflow-hidden">
+            <Image src="/assets/not-found.jpg" loading="lazy" alt="Photo by CHUTTERSNAP" width={640} height={420} />
           </div>
 
           <div className="flex flex-col justify-center items-center sm:items-start md:py-24 lg:py-32">
-            <p className="text-indigo-500 text-sm md:text-base font-semibold uppercase mb-4">Error 404</p>
+            <p className="text-green-500 text-sm md:text-base font-semibold uppercase mb-4">Error 404</p>
             <h1 className="text-gray-800 text-2xl md:text-3xl font-bold text-center sm:text-left mb-2">
               Page not found
             </h1>
@@ -28,7 +23,7 @@ const PageNotFound = () => {
             <nav className="flex sm:block gap-4 sm:space-y-1 md:space-y-2">
               <div>
                 <Link href="/">
-                  <a className="inline-block text-indigo-500 hover:text-indigo-600 active:text-indigo-700 text-sm md:text-base transition duration-100">
+                  <a className="inline-block text-green-500 hover:text-green-600 active:text-green-700 text-sm md:text-base transition duration-100">
                     Home
                   </a>
                 </Link>
@@ -36,7 +31,7 @@ const PageNotFound = () => {
 
               <div>
                 <Link href="/resources">
-                  <a className="inline-block text-indigo-500 hover:text-indigo-600 active:text-indigo-700 text-sm md:text-base transition duration-100">
+                  <a className="inline-block text-green-500 hover:text-green-600 active:text-green-700 text-sm md:text-base transition duration-100">
                     Browse resources
                   </a>
                 </Link>
@@ -44,7 +39,7 @@ const PageNotFound = () => {
 
               <div>
                 <Link href="/resources/request">
-                  <a className="inline-block text-indigo-500 hover:text-indigo-600 active:text-indigo-700 text-sm md:text-base transition duration-100">
+                  <a className="inline-block text-green-500 hover:text-green-600 active:text-green-700 text-sm md:text-base transition duration-100">
                     Request resource
                   </a>
                 </Link>
