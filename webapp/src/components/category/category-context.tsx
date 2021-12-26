cb4c4a0a7553120261230209f5a3bf2468766d55
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-12-26 13:46:54 +0100

    feat(app): create the component to display category tree

---
+++ webapp/src/components/category/category-context.tsx
@@ -0,0 +1,25 @@
+import React, { PropsWithChildren, useContext, useState } from 'react';
+import { Category } from '@/types/model';
+
+export const CategoryContext = React.createContext<Category[]>([]);
+
+const CategoryProvider = ({ children, value }: PropsWithChildren<{ value: Category[] }>) => {
+  return <CategoryContext.Provider value={value}>{children}</CategoryContext.Provider>;
+};
+
+const useCategoryTree = () => {
+  const categories = useContext(CategoryContext);
+
+  const rootCategories = categories.filter((category) => category.parentId === null);
+
+  const findChildren = (categoryParentId: string) => {
+    return categories.filter((category) => category.parentId === categoryParentId);
+  };
+
+  return {
+    rootCategories,
+    findChildren,
+  };
+};
+
+export { CategoryProvider, useCategoryTree };
--- webapp/src/containers/private/categories.tsx
+++ webapp/src/containers/private/categories.tsx
@@ -1,10 +1,68 @@
+import { Category } from '@/types/model';
 import { withPrivateLayout } from '@/components/hof/with-private-layout';
+import CategoryTree from '@/components/category/category-tree';
+import { CategoryProvider, useCategoryTree } from '@/components/category/category-context';
+
+const data: Category[] = [
+  {
+    parentId: null,
+    id: '1',
+    name: 'Cat One',
+    value: 'cat-one',
+    description: 'Desc cat one',
+  },
+  {
+    parentId: null,
+    id: '2',
+    name: 'Cat Two',
+    value: 'cat-two',
+    description: 'Desc cat two',
+  },
+  {
+    parentId: null,
+    id: '3',
+    name: 'Cat Three',
+    value: 'cat-three',
+    description: 'Desc cat three',
+  },
+  {
+    parentId: '1',
+    id: '11',
+    name: 'Cat One One',
+    value: 'cat-one-one',
+    description: 'Desc cat one one',
+  },
+  {
+    parentId: '3',
+    id: '31',
+    name: 'Cat Three One',
+    value: 'cat-three-one',
+    description: 'Desc cat three one',
+  },
+];
+
+const CategoryView = () => {
+  const { rootCategories } = useCategoryTree();
+
+  return (
+    <div className="py-5 w-3/5 mx-auto">
+      <div className="mt-12 flex flex-col w-full items-center justify-center bg-white rounded-lg shadow">
+        <div className="px-4 py-5 sm:px-6 border-b w-full">
+          <h1 className="text-3xl font-medium text-gray-900">Categories Tree</h1>
+        </div>
+        <div className="p-4 w-full border-t border-gray-100">
+          <CategoryTree items={rootCategories} isRootLevel={true} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const CategoriesList = () => {
   return (
-    <div className="py-5">
-      <h1 className="text-4xl font-bold">Categories List</h1>
-    </div>
+    <CategoryProvider value={data}>
+      <CategoryView />
+    </CategoryProvider>
   );
 };
 
