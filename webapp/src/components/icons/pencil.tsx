1b1c069dee23e2d01e5e9808cf9c7e473cd2e0be
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-12-26 15:28:21 +0100

    feat(app): add event to delete a category

---
+++ webapp/src/components/icons/pencil.tsx
@@ -0,0 +1,24 @@
+import { IconProps } from '@/types/common';
+
+const PencilIcon = ({ height = 24, width = 24, className }: IconProps) => {
+  return (
+    <svg
+      className={className}
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+      />
+    </svg>
+  );
+};
+
+export default PencilIcon;
--- webapp/src/types/common.ts
+++ webapp/src/types/common.ts
@@ -1,4 +1,5 @@
 export type IconProps = {
+  className?: string;
   width?: number;
   height?: number;
 };
