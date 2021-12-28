10ae7ce5e26e5cc8eaaff79554f4218b0e404874
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-12-29 00:25:08 +0100

    feat(app): create the input to request the resource

--- webapp/src/pages/resources/request.tsx
+++ webapp/src/pages/resources/request.tsx
@@ -1,9 +1,32 @@
 import dynamic from 'next/dynamic';
+import prisma from '@/lib/prisma';
+import { CategoryTree } from '@/types/common';
+import { formatCategoryToCategoryChoice } from '@/utils/forms';
 
 const RequestResource = dynamic(() => import('@/containers/resources/request'));
 
-const RequestResourcePage = () => {
-  return <RequestResource />;
+type Props = {
+  categories: CategoryTree[];
 };
+
+const RequestResourcePage = ({ categories }: Props) => {
+  return <RequestResource categories={categories} />;
+};
+
+export async function getServerSideProps() {
+  const categories = await prisma.category.findMany({
+    select: {
+      id: true,
+      name: true,
+      value: true,
+      parentId: true,
+      description: true,
+    },
+  });
+
+  const transformedCategories = formatCategoryToCategoryChoice(categories);
+
+  return { props: { categories: transformedCategories } };
+}
 
 export default RequestResourcePage;
