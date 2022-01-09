8601bccc7b92f3cfec6c439ff04de162871b36fd
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-01-09 22:05:50 +0100

    feat(app): build responsive design for the header

---
+++ webapp/src/components/icons/menu.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from '@/types/common';
+
+const MenuIcon = ({ className, width = 24, height = 24 }: IconProps) => {
+  return (
+    <svg
+      className={className}
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
+  );
+};
+
+export default MenuIcon;
