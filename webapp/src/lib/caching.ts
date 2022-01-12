5f7114cae00e05b448a94d8f0ba5541bb18ceeda
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-01-12 23:58:57 +0100

    feat(app): update fetch resources to retrieve data from cache first

--- webapp/src/lib/caching.ts
+++ webapp/src/lib/caching.ts
@@ -1,49 +1,56 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 const API_BASE_URL = process.env.REDIS_API_URL;
 const API_AUTH_TOKEN = process.env.REDIS_API_TOKEN;
 const CACHE_EXPIRATION_TIME = 30 * 60;
 
-const createHttpClient = (apiBaseURL?: string, apiToken?: string) => {
-  if (!apiBaseURL || !apiToken) {
-    throw new Error('Api base URL or API authorization token is missing!');
+class CacheClient {
+  private httpClient: AxiosInstance;
+
+  constructor() {
+    if (!API_BASE_URL || !API_AUTH_TOKEN) {
+      throw new Error('Api base URL or API authorization token is missing!');
+    }
+
+    this.httpClient = axios.create({
+      baseURL: API_BASE_URL,
+    });
+
+    this.httpClient.defaults.headers.common['Accept'] = 'application/json';
+    this.httpClient.defaults.headers.common['Content-Type'] = 'application/json';
+    this.httpClient.defaults.headers.common['Authorization'] = `Bearer ${API_AUTH_TOKEN}`;
+    this.httpClient.defaults.timeout = 10000;
   }
 
-  const instance = axios.create({
-    baseURL: apiBaseURL,
-  });
+  private cacheKey(value: string): string {
+    return `tcg_${value}`;
+  }
 
-  instance.defaults.headers.common['Accept'] = 'application/json';
-  instance.defaults.headers.common['Content-Type'] = 'application/json';
-  instance.defaults.headers.common['Authorization'] = apiToken;
-  instance.defaults.timeout = 10000;
+  async findData<T>(key: string): Promise<T | null> {
+    const response = await this.httpClient.get<{ result: T | null }>(`/get/${this.cacheKey(key)}`).catch((error) => {
+      // TODO capture exception with sentry
+      console.log(error.message);
 
-  return instance;
-};
+      return { data: { result: null } };
+    });
 
-const findData = async <T>(key: string): Promise<T | null> => {
-  const httpClient = createHttpClient(API_BASE_URL, API_AUTH_TOKEN);
+    return response.data.result;
+  }
 
-  const response = await httpClient.get<T>(`/get/${key}`).catch((error) => {
-    // TODO capture exception with sentry
-    console.log(error.message);
+  async cacheData<T>(key: string, data: T): Promise<void> {
+    const dataAsString = JSON.stringify(data);
 
-    return { data: null };
-  });
+    await this.httpClient
+      .post<T>(
+        '',
+        `["SET", "${this.cacheKey(key)}", "${dataAsString.replaceAll('"', '"')}", "EX", ${CACHE_EXPIRATION_TIME}]`,
+      )
+      .catch((error) => {
+        console.log(error);
 
-  return response.data;
-};
+        throw new Error('Fail to cache the data!');
+      });
+  }
+}
 
-const cacheData = async <T>(key: string, data: T): Promise<void> => {
-  const dataAsString = JSON.stringify(data);
-
-  const httpClient = createHttpClient(API_BASE_URL, API_AUTH_TOKEN);
-
-  await httpClient.post<T>('', `["SET", ${key}, ${dataAsString}, "EX", ${CACHE_EXPIRATION_TIME}]`).catch((error) => {
-    console.log(error.message);
-
-    return { data: null };
-  });
-};
-
-export { findData, cacheData };
+export default CacheClient;
