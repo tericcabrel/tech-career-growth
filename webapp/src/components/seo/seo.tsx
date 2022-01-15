5f87026cb6c39662354481ee49229d2e0ba37448
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-01-15 23:30:58 +0100

    feat(app): configure the seo

---
+++ webapp/src/components/seo/seo.tsx
@@ -0,0 +1,56 @@
+import { NextSeo, DefaultSeo } from 'next-seo';
+
+type SeoProps = {
+  path?: string;
+  title: string;
+};
+
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
+
+const GlobalSeo = () => {
+  const description = 'Find the right resource to grow your career in tech.';
+  const title = 'Tech Career Growth';
+
+  return (
+    <DefaultSeo
+      title={title}
+      description={description}
+      canonical={baseUrl}
+      additionalMetaTags={[
+        { content: 'Alex Chiou and Rahul Pandey', property: 'author' },
+        {
+          content: 'tech career growth, improve resume, build side projects, productivity, community',
+          property: 'keywords',
+        },
+      ]}
+      openGraph={{
+        description,
+        images: [
+          {
+            alt: 'Homepage image alt',
+            height: 575,
+            url: `${baseUrl}/assets/img/og.png`,
+            width: 1245,
+          },
+        ],
+        locale: 'en-US',
+        site_name: 'Tech Career Growth',
+        title,
+        url: baseUrl,
+      }}
+      twitter={{
+        cardType: 'summary_large_image',
+        handle: '@rpandey1234',
+        site: '@rpandey1234',
+      }}
+    />
+  );
+};
+
+const PageSeo = ({ path, title }: SeoProps) => {
+  const url = path ? `${baseUrl}${path}` : baseUrl;
+
+  return <NextSeo title={title} canonical={url} />;
+};
+
+export { GlobalSeo, PageSeo };
--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
+import router from 'next/router';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MainLayout from '@/components/layout/main';
 import type { ExtendedAppProps } from '@/types/app';
 import AuthGuard from '@/components/auth/auth-guard';
+import { GlobalSeo } from '@/components/seo/seo';
+import { pageView } from '@/utils/gtag';
 
 import 'react-toastify/dist/ReactToastify.css';
 import '@/styles/global.css';
@@ -11,10 +15,23 @@ import '@/styles/global.css';
 const queryClient = new QueryClient({ defaultOptions: { queries: { retry: 0 } } });
 
 const CustomApp = ({ Component, pageProps: { session, ...pageProps } }: ExtendedAppProps) => {
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageView(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <SessionProvider session={session} refetchInterval={5 * 60}>
       <QueryClientProvider client={queryClient}>
         <MainLayout>
+          <GlobalSeo />
           {Component.auth ? (
             <>
               {/* @ts-ignore */}
