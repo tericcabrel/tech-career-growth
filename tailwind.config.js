716eeb078c96e99b94e580de23ba844e0e905bd9
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-01-26 23:56:53 +0100

    style(app): upgrade tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
