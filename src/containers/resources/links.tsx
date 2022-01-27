0605e4ead50c8e00b8691784ba529e380b3110c5
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-01-27 23:00:44 +0100

    feat(resources): improve the display of markdown content

--- src/containers/resources/links.tsx
+++ src/containers/resources/links.tsx
@@ -3,6 +3,8 @@ import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
+import rehypeExternalLinks from 'rehype-external-links';
+import emoji from 'node-emoji';
 
 import withPublicLayout from '@/components/hof/with-public-layout';
 
@@ -11,16 +13,19 @@ type Props = {
 };
 
 const ResourceLinks = ({ content }: Props) => {
+  const onEmojiMissing = (name: string) => {
+    return name;
+  };
+
   return (
     <div className="py-2">
-      <div className="w-2/3 p-6 mx-auto xs:w-full">
-        <div
-          className="prose
-          prose-h3:flex prose-a:flex prose-a:items-center prose-img:my-0
-          "
-        >
-          <ReactMarkdown rehypePlugins={[rehypeRaw, rehypeSlug, rehypeAutolinkHeadings]} remarkPlugins={[remarkGfm]}>
-            {content}
+      <div className="w-2/3 p-6 mb-6 mx-auto xs:w-full" id="resource-links">
+        <div className="prose prose-h3:flex prose-img:my-0">
+          <ReactMarkdown
+            rehypePlugins={[rehypeRaw, rehypeSlug, rehypeAutolinkHeadings, [rehypeExternalLinks, { target: '_blank' }]]}
+            remarkPlugins={[remarkGfm]}
+          >
+            {emoji.emojify(content, onEmojiMissing)}
           </ReactMarkdown>
         </div>
       </div>
