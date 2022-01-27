68e9fb3889756d3abb59e1d64a38a065c129491c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-01-27 23:33:22 +0100

    feat(resources): add table of contents in the resources links page

--- src/containers/resources/links.tsx
+++ src/containers/resources/links.tsx
@@ -5,6 +5,7 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 import rehypeExternalLinks from 'rehype-external-links';
 import emoji from 'node-emoji';
+import toc from '@jsdevtools/rehype-toc';
 
 import withPublicLayout from '@/components/hof/with-public-layout';
 
@@ -20,9 +21,15 @@ const ResourceLinks = ({ content }: Props) => {
   return (
     <div className="py-2">
       <div className="w-2/3 p-6 mb-6 mx-auto xs:w-full" id="resource-links">
-        <div className="prose prose-h3:flex prose-img:my-0">
+        <div className="prose prose:max-w-full prose-h3:flex prose-img:my-0">
           <ReactMarkdown
-            rehypePlugins={[rehypeRaw, rehypeSlug, rehypeAutolinkHeadings, [rehypeExternalLinks, { target: '_blank' }]]}
+            rehypePlugins={[
+              rehypeRaw,
+              rehypeSlug,
+              rehypeAutolinkHeadings,
+              [rehypeExternalLinks, { target: '_blank' }],
+              toc,
+            ]}
             remarkPlugins={[remarkGfm]}
           >
             {emoji.emojify(content, onEmojiMissing)}
