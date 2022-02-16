8ec48fe4967d5d550e52bc4ab85e8632d91d3af5
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2022-02-16 23:04:19 +0100

    chore(resources): clean useless functions + remove unused package

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -1,22 +1,11 @@
 import dynamic from 'next/dynamic';
-import matter from 'gray-matter';
-import axios from 'axios';
-import markdownToHtml from '@/utils/markdownToHTML';
 import { RESOURCE_LINKS_URL } from '@/utils/constants';
+import { fetchContentFromGitHub } from '@/utils/http-client';
 
 const ResourceLinks = dynamic(() => import('@/containers/resources/links'));
 
-const fetchContentFromGitHub = async (url: string): Promise<string> => {
-  const response = await axios.get<string>(url).catch(() => ({ data: '## No content for the moment.' }));
-
-  return response.data;
-};
-
 export async function getStaticProps() {
   const markdownText = await fetchContentFromGitHub(RESOURCE_LINKS_URL);
-  /*const { content } = matter(markdownText);
-
-  const htmlContent: string = await markdownToHtml(content || '');*/
 
   return {
     props: { content: markdownText },
--- src/utils/http-client.ts
+++ src/utils/http-client.ts
@@ -1,7 +1,13 @@
-import { AxiosError } from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export const getErrorMessage = (error: unknown): string | undefined => {
   const axiosError = error as AxiosError;
 
   return axiosError.response?.data.message;
+};
+
+export const fetchContentFromGitHub = async (url: string): Promise<string> => {
+  const response = await axios.get<string>(url).catch(() => ({ data: '## No content for the moment.' }));
+
+  return response.data;
 };
